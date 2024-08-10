52b15e8e4c2d812db8aee9518e7a0cc76b79c3e6
anabaya <anab.del@hotmail.com>
2024-08-10 20:56:52 +0200

    Initialisation projet

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: "tsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+        ".ts": "tsx",
+      },
+    },
+  },
+  resolve: {
+    alias: { "@": resolve(__dirname, "./src") },
+  }
+});
